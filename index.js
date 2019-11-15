a7deca028b43e4c9af9613660b4e6b2cebde8121
Shane <shanedavenport15@gmail.com>
2019-11-15 16:19:28 -0700

    I nuked it all

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 5000;
+var app = express();
 
-express()
-    .use(express.static(path.join(__dirname, "public")))
-    .set("views", path.join(__dirname, "views"))
-    .set("view engine", "ejs")
-    .get("/", (req, res) => {
-        res.sendFile("index.html", { root: __dirname + "/public/Prove1"});
-    })
-    .get("/calc", (req, res) => {
-        res.sendFile("index.html", { root: __dirname + "/public/Prove1"});
-    })
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.set("port", process.env.PORT || 5000)
+   .use(express.static(__dirname + "/public"))
+   .set("views", __dirname + "/views")
+   .set("view engine", "ejs")
+   .get("/", function(req, res){
+      res.sendFile("form.html", { root: __dirname + "/public"});
+   })
+   .get("/game", function(req, res) {
+      var player = req.query.player_choice;
+      var username = req.query.username;
+      var cpu = "rock";
+      var winner = username;
+      var stuff = {player: player, cpu: cpu, winner: winner};
+      res.render('results', stuff);
+   })
+   .listen(app.get("port"), function() {
+      console.log("Listening on port: " + app.get("port"));
+   })
