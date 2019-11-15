3d3db59a3ab7fbd3b5602f6a469d350996c2f104
Shane <shanedavenport15@gmail.com>
2019-11-15 09:30:22 -0700

    I don't know what I'm doing

--- index.js
+++ index.js
@@ -2,18 +2,11 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
-var gameEngine = require("./gameEngine.js");
-
 express()
     .use(express.static(path.join(__dirname, "public")))
     .set("views", path.join(__dirname, "views"))
     .set("view engine", "ejs")
     .get("/", (req, res) => {
-        res.sendFile("index.html", { root: __dirname + "/public/Prove1"});
+        res.sendFile("index.ejs", { root: __dirname + "/public/Prove1"});
     })
-    .get("/", (req, res) => {
-        let weight = req.query.weight;
-        res.sendFile(`result.ejs?weight=${weight}`, { root: __dirname + "/public/Prove1"});
-    })
-    .get("/game", gameEngine.playGame)
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
