19c10e9568134b6ed351d42d95652da55a0441c7
Shane <shanedavenport15@gmail.com>
2019-11-14 21:11:12 -0700

    More Files Yay

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+var gameEngine = require("./gameEngine.js");
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, "public")))
+    .set("views", path.join(__dirname, "views"))
+    .set("view engine", "ejs")
+    .get("/", (req, res) => {
+        res.sendFile("index.html", { root: __dirname + "/public/Prove1"});
+    })
+    .get("/", (req, res) => {
+        let weight = req.query.weight;
+        res.sendFile(`result.ejs?weight=${weight}`, { root: __dirname + "/public/Prove1"});
+    })
+    .get("/game", gameEngine.playGame)
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
