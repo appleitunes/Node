7aec9bc2c54d6ac4bdb1912eda2485e9238dc41d
Shane <shanedavenport15@gmail.com>
2019-11-19 20:39:37 -0700

    Return JSON

--- functions/getPerson.js
+++ functions/getPerson.js
@@ -8,18 +8,4 @@ function getPerson(req, res) {
     res.end();
 }
 
-// function query() {
-//     pool.query(sql, function(err, result) {
-//         // If an error occurred...
-//         if (err) {
-//             console.log("Error in query: ")
-//             console.log(err);
-//         }
-
-//         // Log this to the console for debugging purposes.
-//         console.log("Back from DB with result:");
-//         console.log(result.rows);
-//     });  
-// }
-
 module.exports = {getPerson: getPerson};
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const calculateRate = require('./functions/calculateRate.js');
 var app = express();
 
 const { Pool } = require("pg");
@@ -7,20 +6,22 @@ const connectionString = process.env.DATABASE_URL || "postgres://qkoefgxxlcvrpi:
 const pool = new Pool({connectionString: connectionString});
 
 app.set("port", process.env.PORT || 5000)
-   .use(express.static(__dirname + "/public"))
-   .set("views", __dirname + "/views")
-   .get("/", (req, res) => {
-      res.sendFile("home.html", { root: __dirname + "/public"});
-   })
-   .get("/getRate", calculateRate.calculateRate)
-   .get("/getPerson", (req, res) => {
-      let sql = "SELECT * FROM PERSON;";
-      pool.query(sql, (err, result) => {
-         let string = JSON.stringify(result.rows);
-         res.write(string);
-         res.end();
-      });  
-   })
-   .listen(app.get("port"), () => {
-      console.log("Listening on port: " + app.get("port"));
-   })
+.use(express.static(__dirname + "/public"))
+.set("views", __dirname + "/views")
+.get("/", (req, res) => {
+   res.sendFile("home.html", { root: __dirname + "/public"});
+})
+.get("/getPerson", getPerson)
+.listen(app.get("port"), () => {
+   console.log("Listening on port: " + app.get("port"));
+})
+
+function getPerson(req, res) {
+   res.header("Content-Type",'application/json');
+   let sql = "SELECT * FROM PERSON;";
+   pool.query(sql, (err, result) => {
+      let string = JSON.stringify(result.rows);
+      res.write(string);
+      res.end();
+   });  
+}
