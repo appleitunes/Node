a906d26784c0c6de76fc4b3746d6d7c01cefba31
Shane <shanedavenport15@gmail.com>
2019-11-19 21:34:39 -0700

    I was using get instead of post. Oops

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var app = express();
+app.use(express.static(__dirname + '/public'));
 
 const { Pool } = require("pg");
 const connectionString = process.env.DATABASE_URL || "postgres://qkoefgxxlcvrpi:c10a493d57075b82c1d9b6ed226a83a30ba80cd59b2a94fb2055e38d7cff557c@ec2-174-129-253-162.compute-1.amazonaws.com:5432/dehq73vk2n1uod?ssl=true";
@@ -7,11 +8,10 @@ const pool = new Pool({connectionString: connectionString});
 
 app.set("port", process.env.PORT || 5000)
 .use(express.static(__dirname + "/public"))
-.set("views", __dirname + "/views")
 .get("/", (req, res) => {
    res.sendFile("home.html", { root: __dirname + "/public"});
 })
-.get("/getPerson", getPerson)
+.post("/getPerson", getPerson)
 .listen(app.get("port"), () => {
    console.log("Listening on port: " + app.get("port"));
 })
@@ -23,5 +23,5 @@ function getPerson(req, res) {
       let string = JSON.stringify(result.rows);
       res.write(string);
       res.end();
-   });  
+   });
 }
--- public/javascript/home.js
+++ public/javascript/home.js
@@ -2,5 +2,8 @@ window.onload = () => {
     httpCall("getPerson")
     .then((data) => {
         document.getElementById("content").innerHTML = data;
+    })
+    .catch((error) => {
+        document.getElementById("content").innerHTML = error;
     });
 };
