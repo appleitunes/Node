2c50728686f3b5181d77f4805c20132064de74d5
Shane <shanedavenport15@gmail.com>
2019-11-20 22:05:36 -0700

    dsf

--- index.js
+++ index.js
@@ -23,19 +23,16 @@ function getData(req, res) {
    let keys = req.query.keys;
    let table = req.query.table;
 
-   let sql = `SELECT ${keys} FROM ${table};`;
+   let sql = `SELECT * FROM ACCOUNT;`;
 
-   res.write(sql);
-   res.end();
-
-   // pool.query(sql, (err, result) => {
-   //    if (err) {
-   //       res.write(err);
-   //       res.end();
-   //    }
+   pool.query(sql, (err, result) => {
+      if (err) {
+         res.write(err);
+         res.end();
+      }
  
-   //    let JSONData = JSON.stringify(result.rows);
-   //    res.write(JSONData);
-   //    res.end();
-   // });
+      let JSONData = JSON.stringify(result.rows);
+      res.write(JSONData);
+      res.end();
+   });
 }
