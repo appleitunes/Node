ba59a2cb8ff934085a484c4bd1dda847180dbf42
Shane <shanedavenport15@gmail.com>
2019-11-21 12:30:44 -0700

    Added a function to return all cards in deck

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.set("port", process.env.PORT || 5000)
    res.sendFile("home.html", { root: __dirname + "/public"});
 })
 .post("/getDecks", getDecks)
-.post("/getCards", getCards)
 .listen(app.get("port"), () => {
    console.log("Listening on port: " + app.get("port"));
 });
@@ -20,31 +19,30 @@ app.set("port", process.env.PORT || 5000)
 function getDecks(req, res) {
    let accountID = req.query.account;
 
-   let sql = `SELECT * FROM DECK WHERE owner_account=${accountID}`;
+   let SQL = `SELECT * FROM DECK WHERE owner_account=${accountID}`;
 
-   pool.query(sql, (err, result) => {
+   pool.query(SQL, (err, result) => {
       if (err) {
          res.write("0");
          res.end();
       }
- 
-      res.write(JSON.stringify(result.rows));
-      res.end();
-   });
-}
+      else {
+         let rows = result.rows;
 
-function getCards(req, res) {
-   let deckID = req.query.deck;
-
-   let sql = `SELECT * FROM CARD WHERE owner_deck=${deckID};`;
-
-   pool.query(sql, (err, result) => {
-      if (err) {
-         res.write("0");
-         res.end();
+         for (let i = 0; i < rows.length; i++) {  
+            let row = rows[i];
+            row.cards = [];
+            let newSQL = `SELECT * FROM CARD WHERE owner_deck=${element["deck_id"]}`;
+   
+            pool.query(newSQL, (err, result) => {
+               rows[i].cards.push(JSON.stringify(result));
+            });
+            
+            if (i == rows.length - 1) { 
+               res.write(JSON.stringify(rows));
+               res.end();
+            }
+         }
       }
- 
-      res.write(result);
-      res.end();
    });
 }
