e2c8769e906fea6d374bc1ece8e213e117a0ba33
Shane <shanedavenport15@gmail.com>
2019-11-22 11:55:29 -0700

    sdffsdf

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,33 +1,33 @@
-// function httpCall(url) {
-//     return new Promise((resolve, reject) => {
-//         let xHTTP = new XMLHttpRequest();
-
-//         xHTTP.onreadystatechange = () => { 
-//             if (xHTTP.readyState == 4) {
-//                 if (xHTTP.status == 200) {
-//                     resolve(xHTTP.responseText);
-//                 }
-//                 else {
-//                     reject("Error");
-//                 }
-//             }
-//         }
-
-//         xHTTP.open("POST", url, true);
-//         xHTTP.send(null);
-//     });
-// }
-
 function httpCall(url) {
     return new Promise((resolve, reject) => {
-        fetch(url, {
-            method: 'post',
-        })
-        .then((response) => {
-            resolve(response);
-        })
-        .catch((error) => {
-            reject(error);
-        });
+        let xHTTP = new XMLHttpRequest();
+
+        xHTTP.onreadystatechange = () => { 
+            if (xHTTP.readyState == 4) {
+                if (xHTTP.status == 200) {
+                    resolve(xHTTP.responseText);
+                }
+                else {
+                    reject("Error");
+                }
+            }
+        }
+
+        xHTTP.open("POST", url, true);
+        xHTTP.send(null);
     });
 }
+
+// function httpCall(url) {
+//     return new Promise((resolve, reject) => {
+//         fetch(url, {
+//             method: 'post',
+//         })
+//         .then((response) => {
+//             resolve(response);
+//         })
+//         .catch((error) => {
+//             reject(error);
+//         });
+//     });
+// }
