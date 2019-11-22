e88137a59c3668c2a0fa4d9f00d3643143075fc4
Shane <shanedavenport15@gmail.com>
2019-11-22 15:44:21 -0700

    sdjfbsdjb

--- public/javascript/study.js
+++ public/javascript/study.js
@@ -1,17 +1,35 @@
 function loadRest() {
+    loadDecks(1);
+};
+
+function loadDecks(accountID) {
     httpCall("../html/deck.html") 
     .then((template) => {
-        getDecks(1)
+        getDecks(accountID)
         .then((decks) => {
             document.getElementById("list-container").innerHTML = "";
             for (i in decks) {
                 let newDeck = decks[i];
                 let templateCopy = template.replace("$title", newDeck.title);
+                templateCopy = template.replace("$deckID", newDeck.deck_id);
                 document.getElementById("list-container").innerHTML += templateCopy;
             }
+        })
+        .catch((error) => {
+            document.getElementById("list-container").innerHTML = error;
         });
     });
-};
+}
+
+function loadCards(deckID) {
+    getCards(deckID)
+    .then((cards) => {
+        alert(JSON.stringify(cards));
+    })
+    .catch((error) => {
+        alert(error);
+    })
+}
 
 function getDecks(accountID) {
     return new Promise((resolve, reject) => {
