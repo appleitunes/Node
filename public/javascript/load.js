74da061fc0197c8b46722c00741c9e838665b9dd
Shane <shanedavenport15@gmail.com>
2019-12-07 21:29:36 -0700

    sdf

--- public/javascript/load.js
+++ public/javascript/load.js
@@ -1,4 +1,6 @@
 window.onload = () => {
+    document.body.style.minHeight = window.screen.availHeight + "px";
+    
     httpCall("../html/nav.html")
     .then((template) => {
         document.getElementById("navbar").innerHTML = template;
