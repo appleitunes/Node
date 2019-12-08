7d783ae11ae07917d6f5492fc00f9419a5f0f4dd
Shane <shanedavenport15@gmail.com>
2019-12-07 21:40:01 -0700

    sdf

--- public/javascript/load.js
+++ public/javascript/load.js
@@ -1,14 +1,9 @@
 window.onload = () => {
     document.body.style.minHeight = window.screen.availHeight + "px";
-    
+
     httpCall("../html/nav.html")
     .then((template) => {
         document.getElementById("navbar").innerHTML = template;
-    });
-
-    httpCall("../html/footer.html")
-    .then((template) => {
-        document.getElementById("footer-content").innerHTML = template;
     });
 
     if (typeof loadRest !== "undefined") { 
